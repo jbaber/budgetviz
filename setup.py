4ad1a6c8df83415725b89df925c3c04ee6909c53
John Baber <git@frundle.com>
2014-11-13 22:58:18 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setuptools.setup(name='budgetviz',
   packages=['budgetviz'],
   entry_points = {
     'console_scripts': ['budgetviz=budgetviz.command_line:main'],
-  }
+  },
+  install_requires=[
+    'sqlalchemy',
+    'yaml',
+    'webbrowser',
+    'docopt',
+  ],
 )
